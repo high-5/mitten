60864ceeb9e9e2eab7d01caad0bd23fc9d32b149
Max <max@teamatb.com>
2015-05-26 20:51:09 -0400

    Add non-complex templating/rendering of initial book list

--- src/cljs/mitten/core.cljs
+++ src/cljs/mitten/core.cljs
@@ -7,19 +7,33 @@
 
 (def url "https://api.github.com/repos/high-5/discussion/issues/")
 
+(defn resource-format [{:keys [body user] :as comment}]
+  (str body " - " (:login user)))
+
+(defn resource-view [resource owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (resource-format resource)))))
+
 (defn main []
   (om/root
     (fn [app owner]
       (reify
         om/IRender
         (render [_]
-          (dom/h1 nil "TODO"))))
+
+          (dom/div {:className "books"}
+            (apply dom/ul nil
+              (om/build-all resource-view (:books app)))))))
     app-state
     {:target (. js/document (getElementById "app"))}))
 
+
+
 (defn get-issue [issue-number issue-key]
   (let [uri (str url issue-number "/comments")]
-    (GET uri {:handler #(swap! app-state assoc issue-key (map :body %)) :response-format :json :keywords? true})))
+    (GET uri {:handler #(swap! app-state assoc issue-key %) :response-format :json :keywords? true})))
 
 
 (get-issue 1 :books)
