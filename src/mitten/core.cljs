0f42d20eaf8e42ca7cd38a514baadcffc2c5cff9
Max Mechanic <max@philo.com>
2015-06-06 09:59:13 -0400

    Simplify Om views, move data transform to fetch handler, organize funcs

--- src/mitten/core.cljs
+++ src/mitten/core.cljs
@@ -5,9 +5,14 @@
             [markdown.core :refer [md->html]]
             [dommy.core :as dommy :refer-macros [sel sel1]]))
 
+;; keep atom up top
+
 (defonce app-state (atom {
                           :books []
                           :projects []}))
+
+
+;; data fetch + transform
 
 (def url "https://api.github.com/repos/high-5/discussion/issues/")
 
@@ -19,21 +24,38 @@
     (aset el "innerHTML" (md->html md))
     (map get-link-data (sel el :a))))
 
-(defn link-view [{:keys [user link-data]} owner]
-  (reify
-    om/IRender
-    (render [this]
-      (dom/li nil
-        (dom/a #js {:href (:href link-data)} (:text link-data))
-        (dom/span nil " - " )
-        (dom/a #js {:href (:html_url user)} (:login user))))))
+(defn structure-links [{:keys [user body]}]
+  (map #(zipmap [:user :link-data] [user %])
+    (md->links body)))
 
-(defn resource-view [{:keys [body user] :as resource} owner]
+(defn build-links [data]
+  (filter not-empty
+    (map structure-links data)))
+
+(defn get-issue [issue-number issue-key]
+  (let [uri (str url issue-number "/comments")]
+    (GET uri {
+      :handler #(swap! app-state assoc issue-key (build-links %))
+      :response-format :json :keywords? true})))
+
+;; om setup
+
+(defn link-view [{:keys [user link-data]} owner]
+  (let [{:keys [href text]} link-data {:keys [html_url login]} user]
+    (reify
+      om/IRender
+      (render [this]
+        (dom/li nil
+          (dom/a #js {:href href} text)
+          (dom/span nil " - " )
+          (dom/a #js {:href html_url} login))))))
+
+(defn resource-view [resource owner]
   (reify
     om/IRender
     (render [this]
       (apply dom/ul nil
-        (om/build-all link-view (map #(zipmap [:user :link-data] [user %])(md->links body)))))))
+        (om/build-all link-view resource)))))
 
 (defn main []
   (om/root
@@ -52,10 +74,7 @@
     {:target (. js/document (getElementById "app"))}))
 
 
-(defn get-issue [issue-number issue-key]
-  (let [uri (str url issue-number "/comments")]
-    (GET uri {:handler #(swap! app-state assoc issue-key %) :response-format :json :keywords? true})))
-
+;; kick off requests
 
 (get-issue 1 :books)
 (get-issue 2 :projects)
